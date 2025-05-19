4d6ee7ee8c65e71d9f4f88512e2c56e31618de6f
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2025-05-19 22:33:24 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, movies, bookings, profile
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from pathlib import Path
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -26,12 +27,14 @@ app.include_router(movies.router, prefix="/movies", tags=["Movies"])
 app.include_router(bookings.router, prefix="/bookings", tags=["Bookings"])
 app.include_router(profile.router, prefix="/profile", tags=["Profile"])
 
-app.mount("/assets", StaticFiles(directory="../frontend/dist", html=True), name="static")
+dist_path = Path(__file__).resolve().parent.parent / "frontend" / "dist"
+
+app.mount("/assets", StaticFiles(directory=dist_path, html=True), name="static")
 
 
 @app.get("/{full_path:path}")
 async def serve_vue_app(full_path: str):
-    index_file = "../frontend/dist" / "index.html"
+    index_file = dist_path / "index.html"
     if index_file.exists():
         return FileResponse(index_file)
     return {"detail": "index.html not found"}
